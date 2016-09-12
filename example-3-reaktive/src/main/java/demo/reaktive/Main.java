4c1662e99aef11ff1be3b1b2db83f7fd3d8061d9
Jan Ypma <jyp@tradeshift.com>
2016-09-12 14:43:54 +0200

    Add clustering support to main as well

--- example-3-reaktive/src/main/java/demo/reaktive/Main.java
+++ example-3-reaktive/src/main/java/demo/reaktive/Main.java
@@ -1,5 +1,8 @@
 package demo.reaktive;
 
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
+
 import akka.actor.ActorSystem;
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
@@ -9,19 +12,21 @@ import demo.reaktive.conversation.ConversationProtocol;
 import demo.reaktive.rest.RootRoute;
 
 public class Main {
-    {
-        ActorSystem system = ActorSystem.create();
+    public Main(int clusterIndex) {
+        Config config = ConfigFactory.parseString("akka.remote.netty.tcp.port=" + (2551 + clusterIndex)).withFallback(ConfigFactory.load());
+        ActorSystem system = ActorSystem.create("ClusterSystem", config);
         Materializer materializer = ActorMaterializer.create(system);
         ConversationProtocol conversationProtocol = new ConversationProtocol(system);
         RootRoute rootRoute = new RootRoute(conversationProtocol);
         
         Http.get(system).bindAndHandle(
             rootRoute.http().flow(system, materializer),
-            ConnectHttp.toHost("127.0.0.1",8173),
+            ConnectHttp.toHost("127.0.0.1", 8173 + clusterIndex),
             materializer);
     }
     
     public static void main(String[] args) {
-        new Main();
+        int clusterIndex = (args.length == 0) ? 0 : Integer.parseInt(args[0]);
+        new Main(clusterIndex);
     }
 }
